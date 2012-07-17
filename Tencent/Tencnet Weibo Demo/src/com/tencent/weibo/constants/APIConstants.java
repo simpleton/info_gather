bb52858e5d9b239eb6563d2eaae762ca30c7f4ca
jamie <jamie@jamie-ThinkPad-T410.(none)>
2012-07-17 23:32:58 +0800

    init tentcent webo demo

---
+++ Tencent/Tencnet Weibo Demo/src/com/tencent/weibo/constants/APIConstants.java
@@ -0,0 +1,8 @@
+package com.tencent.weibo.constants;
+
+public class APIConstants {
+
+    public static final String API_V1_BASE_URL="http://open.t.qq.com/api";
+    
+    private APIConstants(){}
+}
---
+++ Tencent/Tencnet Weibo Demo/src/com/tencent/weibo/constants/OAuthConstants.java
@@ -0,0 +1,16 @@
+package com.tencent.weibo.constants;
+
+public class OAuthConstants {
+
+    public static final String OAUTH_VERSION_1="1.0";
+
+    public static final String APP_FROM_ANDROID_SDK_1="android-sdk-1.0";
+
+    
+    public static final String OAUTH_V1_GET_REQUEST_TOKEN_URL = "http://open.t.qq.com/cgi-bin/request_token";
+    public static final String OAUTH_V1_AUTHORIZE_URL="http://open.t.qq.com/cgi-bin/authorize";
+    public static final String OAUTH_V1_GET_ACCESS_TOKEN_URL = "http://open.t.qq.com/cgi-bin/access_token";
+    
+    
+    private OAuthConstants(){}
+}
